f96e02e1b4048848b68a5c9b00aa75b809bbeb6f
Asher Hwang <dev@hsh.kr>
2023-04-07 03:15:51 +0900

    Update FooterContainer.tsx

--- components/Footer/FooterContainer.tsx
+++ components/Footer/FooterContainer.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const FooterContainer = styled.footer`
   padding: 20px;
+  padding-bottom: 50px;
   border-top: #ddd 1px solid;
   margin-top: 20px;
 `;
